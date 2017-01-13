49776e60627b0d22b6304df59d2215e102642c03
ayo <262869122@qq.com>
2017-01-13 09:54:48 +0800

    Update import

--- core/src/com/ayocrazy/easystage/bean/BeanCreator.java
+++ core/src/com/ayocrazy/easystage/bean/BeanCreator.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScalingViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.sun.xml.internal.bind.v2.model.core.ID;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
