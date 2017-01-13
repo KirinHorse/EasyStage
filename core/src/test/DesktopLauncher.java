820e2d08404824f4a0eba545fdd80acf00d6f64e
ayo <262869122@qq.com>
2017-01-13 10:01:29 +0800

    Update import

--- core/src/test/DesktopLauncher.java
+++ core/src/test/DesktopLauncher.java
@@ -2,7 +2,6 @@ package test;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import test.MainGame;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
