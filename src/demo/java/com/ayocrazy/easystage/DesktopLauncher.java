d1ec4115b5fdb1ebdb067dcb04237761175d213d
ayo <262869122@qq.com>
2017-01-18 12:10:21 +0800

    downgrade to lwjgl2

--- src/demo/java/com/ayocrazy/easystage/DesktopLauncher.java
+++ src/demo/java/com/ayocrazy/easystage/DesktopLauncher.java
@@ -1,11 +1,11 @@
 package com.ayocrazy.easystage;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        new Lwjgl3Application(new MainGame(), config);
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new MainGame(), config);
     }
 }
